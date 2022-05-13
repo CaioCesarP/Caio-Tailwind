aa773e1087aef453ba26be3fa291850862472452
caio.cp <caio.cesar@itarget.com.br>
2022-05-13 10:29:26 -0300

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: "#C5DFFF",
+          escuro: "#061E3C",
+          hover: "#1057B0",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      Keyframes: {
+        wiggle: {
+          "0%, 100%": {
+            transform: "rotate(-10deg)",
+          },
+          "50%": {
+            transform: "rotate(10deg)",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 0.31s ease-in-out infinite",
+      },
+    },
+  },
+};
